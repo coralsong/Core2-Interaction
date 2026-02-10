803cd97f25420a1227b37727a6c187a2ce0533b5
coralsong <songc215@newschool.edu>
2026-02-10 16:14:39 +0000

    ,

--- iteration-3/script.js
+++ iteration-3/script.js
@@ -4,3 +4,18 @@ const output = document.getElementById("output");
 input.addEventListener("input", () => {
   output.textContent = input.value;
 });
+
+
+const text = "The line is made up of an infinite number of points;";
+let i = 0;
+
+function typeText() {
+  if (i < text.length) {
+    document.getElementById("type").textContent += text[i];
+    i++;
+    setTimeout(typeText, 80);
+  }
+}
+
+typeText();
+
